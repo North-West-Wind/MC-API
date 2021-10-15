05d06934b42c2930318c933d191c63bd1e0198be
eve0415 <40632436+eve0415@users.noreply.github.com>
2021-10-15 11:52:14 +0900

    We cannot get Mojang status anymore

--- src/api/Mojang/status.ts
+++ src/api/Mojang/status.ts
@@ -1,20 +1,11 @@
-import axios from 'axios';
-import { MojangStatus } from '../..';
-import { mojangStatus } from '../url';
-
 /**
  * Fetch Mojang's server status
  * @returns Returns each service with status color
  * - Green: No problem
  * - Yellow: Some issues
  * - Red: Probably not working properly
+ * @deprecated The API do not work anymore. Do not use this
  */
-export async function getMojangStatus(): Promise<MojangStatus> {
-    const res = await axios.get<MojangStatus[]>(mojangStatus);
-    return res.data.reduce((result, obj) => {
-        Object.keys(obj).forEach(k => {
-            result[k] = obj[k];
-        });
-        return result;
-    });
+export function getMojangStatus(): Error {
+    return new Error('Broken API');
 }
